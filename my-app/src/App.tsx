d3768fe60f8012804c1ef30a963a6a50d9bfa652
TalSolodar <talsolodar1@gmail.com>
2023-01-06 14:42:08 +0200

    hello world div

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <a>
-          Learn React
+          Hello World
         </a>
       </header>
     </div>
